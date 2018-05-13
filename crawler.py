cf174ff568643203700120929ae1c568da32e5fb
John Wang <ilcf1u@gmail.com>
2018-05-13 12:33:41 -0700

    cleaned comments

--- crawler.py
+++ crawler.py
@@ -1,26 +1,5 @@
-# import requests
-#
-# res = requests.get('http://pala.tw/js-example/')
-# print(res.text)
-
-# from selenium import webdriver
-#
-# driver = webdriver.PhantomJS(executable_path=r'/Users/Jia-anwang/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs')
-# driver.get('http://pala.tw/js-example/')
-# pageSource = driver.page_source
-# print(pageSource)
-#
-# driver.close()
-
-
 import requests
 from bs4 import BeautifulSoup
-# tag = input("請輸入定位元素，class前面加上.，id前面加上# ")
-# res = requests.get('http://pala.tw/class-id-example/')
-# soup = BeautifulSoup(res.text, "lxml")
-#
-# for drink in soup.select('{}'.format(tag)):
-#     print(drink.get_text())
 
 page = requests.get('https://www.theweathernetwork.com/ca/weather/british-columbia/richmond')
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -37,11 +16,6 @@ print(todayTemp.prettify())
 
 
 print("here's Richmond's weather forecast ")
-# print("date:", date)
-# print("weather:", weather)
-# print("temperature:", temp)
-
-# prints out everyday's weather
 for i in range (0,len(forecast_items)):
     weather = forecast_items_weather[i].get_text()
     temp = forecast_items_deg[i].get_text()
